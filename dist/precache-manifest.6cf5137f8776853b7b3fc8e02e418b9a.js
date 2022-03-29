1da79e90fd6ed75b7c6101cb4a57c8ec4c908305
Ahmad Foroughi <pyhp2017@gmail.com>
2022-03-29 13:15:36 -0400

    New build

---
+++ dist/precache-manifest.6cf5137f8776853b7b3fc8e02e418b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f752f47d0c7effa6668",
+    "url": "/admin/css/app.b650bac8.css"
+  },
+  {
+    "revision": "e9908f05e5771638e40913309b784a17",
+    "url": "/admin/fonts/iranSansFarsiNumber.e9908f05.ttf"
+  },
+  {
+    "revision": "549f2d72634aa3d43b526cb04b6160bf",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "2f752f47d0c7effa6668",
+    "url": "/admin/js/app.8ee98475.js"
+  },
+  {
+    "revision": "eac5ff6fc5606f243702",
+    "url": "/admin/js/chunk-vendors.b9a133e6.js"
+  },
+  {
+    "revision": "ca67caf67d54d6e9a331860c95a87548",
+    "url": "/admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin/robots.txt"
+  }
+]);
